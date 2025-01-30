cf6b3376efd94f09dd7ee71e3446ab903dfef8fa
Noah Holloway <noahholloway23@gmail.com>
2025-01-30 08:28:41 -0700

    good do loop example

--- PorgramFlowS25/ProgramFlowS25.vb
+++ PorgramFlowS25/ProgramFlowS25.vb
@@ -1,7 +1,42 @@
-﻿Module ProgramFlowS25
+﻿Option Explicit On
+Option Strict On
+Option Compare Text
+
+
+Module ProgramFlowS25
 
     Sub Main()
+        Dim firstNumber As Integer
 
+        Do 'starts first number at 0. adds 1 to itself and writes to console what its value is
+            Console.WriteLine("In the first loop. Loop While firstNumber <= 3")
+            firstNumber += 1
+            Console.WriteLine($"FirstNumber is {firstNumber}")
+        Loop While firstNumber <= 3 'loops this process until first number is greater than or equal to 3
+
+        Console.WriteLine()
+
+        Do
+            Console.WriteLine("In the second loop. Loop While firstNumber <= 3") 'same as first loop
+            firstNumber += 1
+            Console.WriteLine($"FirstNumber is {firstNumber}")
+        Loop While firstNumber <= 3 'first number is now 5, second loop stops here
+
+        Console.WriteLine()
+
+        Do Until firstNumber >= 3 'evaluation at the beginning of loop, first number is now >3, so loop never runs
+            Console.WriteLine("In the third loop. Loop While firstNumber >= 3")
+            firstNumber += 1
+            Console.WriteLine($"FirstNumber is {firstNumber}")
+        Loop
+
+        Do While firstNumber >= 3
+            Console.WriteLine("In the fourth loop. Loop While firstNumber >= 3") 'loops while first number is greater than or equal to 3
+            firstNumber -= 1 'subtracts 1 each loop
+            Console.WriteLine($"FirstNumber is {firstNumber}") 'displays new first number
+        Loop
+
+        Console.WriteLine()
     End Sub
 
 End Module
